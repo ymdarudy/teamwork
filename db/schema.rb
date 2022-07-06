02e1b5d01dad929235b99c9087fa40cc962d0b25
SHUNSUKE <shunsuke0810@gmail.com>
2022-07-06 18:00:10 +0900

    コンフリクト解消3

--- db/schema.rb
+++ db/schema.rb
@@ -29,5 +29,5 @@ ActiveRecord::Schema.define(version: 2022_07_06_071909) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-  
+
 end
